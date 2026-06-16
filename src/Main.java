c2c1d83c68603b0e92d7ed73246a55afcfcd59e1
rafinharltp <rafinhapersiguelo@gmail.com>
2026-06-15 21:26:42 -0300

    ✨ feat: adiciona classe Main

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import ui.MainFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        javax.swing.SwingUtilities.invokeLater(
+                () -> new MainFrame()
+                        .setVisible(true)
+        );
+    }
+}
