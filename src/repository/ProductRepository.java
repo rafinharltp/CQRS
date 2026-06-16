17583d8919bf267a1d10600887ed22a21d4bdbdf
rafinharltp <rafinhapersiguelo@gmail.com>
2026-06-15 21:23:04 -0300

    ✨ feat: adiciona repository ProductRepository

---
+++ src/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package repository;
+
+import model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductRepository {
+
+    private final List<Product> products =
+            new ArrayList<>();
+
+    public void save(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> findAll() {
+        return new ArrayList<>(products);
+    }
+}
